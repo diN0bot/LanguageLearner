74d6442c84e02cd320f8c0734bad0f75133992c7
lucy <lucy@procrasdonate.com>
2010-06-04 08:43:29 -0700

    more words

--- flashcards/scripts/read_flashcardfile.py
+++ flashcards/scripts/read_flashcardfile.py
@@ -10,9 +10,12 @@ def read_flashcardfile(filename):
     words = line.split(':')
     wordA = words[0]
     wordB = words[1]
+    print wordA, "      ", wordB, "      ", FlashCard.objects.filter(wordA=wordA), 
     if FlashCard.objects.filter(wordA=wordA).count():
+      print "already exists A"
       continue
     if FlashCard.objects.filter(wordB=wordB).count():
+      print "already exists B"
       continue
     FlashCard.add(wordA, wordB)
   f.close()
